dd2d1976923c7915b195451392aa27cfb7235372
moigarcia00 <moisesgarsanz@gmail.com>
2026-09-23 09:27:22 +0200

    feat: String manipulation

--- main.py
+++ main.py
@@ -21,3 +21,28 @@ cantidad_numerica = int("5")
 # Punto 4
 print(type(identificador))
 print(type(precio_numerico))
+
+
+
+
+
+#Punto 5
+texto_sucio = "   figura coleccionable usada   "
+
+# Quitar espacios en blanco a los lados (.strip())
+texto_limpio = texto_sucio.strip()
+
+# Convertir formatos de mayúsculas/minúsculas
+en_mayusculas = texto_limpio.upper()
+en_minusculas = texto_limpio.lower()
+en_titulo = texto_limpio.title()
+
+# Reemplazar palabras (.replace())
+texto_modificado = texto_limpio.replace("usada", "certificada")
+
+# Separar una cadena por comas
+etiquetas = "retro,anime,edicion_limitada".split(",")
+
+# Concatenación e Interpolación
+concatenado = "Pieza: " + texto_limpio + " - Precio: " + str(precio)
+interpolado = f"Pieza: {texto_limpio} - Precio: ${precio:.2f}"
